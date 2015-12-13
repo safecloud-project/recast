a1e48c8d8b398939abe5bd61d1b7c878da4bc910
Dorian Burihabwa <dorian@burihabwa.com>
2015-12-13 21:14:32 +0100

    routes/index.js linting cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,10 @@
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
 var rawBody = require('raw-body');
 var redis = require('redis');
 var router = express.Router();
 var typer = require('media-typer');
 
-function RS(messageLength, errorCorrectionLength) {
-  'use strict';
-	var dataLength = messageLength - errorCorrectionLength;
-	var encoder = new rs.ReedSolomonEncoder(rs.GenericGF.AZTEC_DATA_8());
-	var decoder = new rs.ReedSolomonDecoder(rs.GenericGF.AZTEC_DATA_8());
-	return {
-		dataLength: dataLength,
-		messageLength: messageLength,
-		errorCorrectionLength: errorCorrectionLength,
-
-		encode : function (message) {
-			encoder.encode(message, errorCorrectionLength);
-		},
-
-		decode: function (message) {
-			decoder.decode(message, errorCorrectionLength);
-		}
-	};
-}
-
-router.get('/(:path)', function(req, res, next) {
+router.get('/(:path)', function(req, res) {
   'use strict';
   var redisClient = redis.createClient({'return_buffers': true});
   redisClient.get(req.path, function (error, data) {
@@ -61,7 +39,7 @@ router.put('/(:path)', rawBodyUpload, function (req, res) {
   var redisClient = redis.createClient();
   redisClient.set(req.path, req.data, function (error) {
     if (error) {
-        return res.status(error.status || 500).send(error.message);
+      return res.status(error.status || 500).send(error.message);
     }
     redisClient.end();
     res.status(200).send('OK');
