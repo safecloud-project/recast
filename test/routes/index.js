5b75279cf9881f94018efee91b0fdc17e901b262
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2015-12-14 10:37:48 +0100

    Decouple tests from redis database
    Replaced function calls to the redis database with stubs for better
    decoupling.

--- test/routes/index.js
+++ test/routes/index.js
@@ -8,19 +8,55 @@ var app = require(path.join(__dirname, '../../app'));
 
 describe('GET /:file', function () {
   'use strict';
+  
+  before(function () {
+    sinon.stub(redis.RedisClient.prototype, 'get', function (key, callback) {
+      callback(null, null);
+    });
+  });
+  
+  after(function () {
+    if (typeof redis.RedisClient.prototype.get.restore === 'function') {
+      redis.RedisClient.prototype.get.restore();
+    }
+  });
+
   it('should return a 404 if the file does not exist', function (done) {
-    supertest(app).get('/doesnotexist').expect(404).end(done);
+    var initialCallCount = redis.RedisClient.prototype.get.callCount;
+    supertest(app).get('/doesnotexist').expect(404).expect(function () {
+      assert.strictEqual(redis.RedisClient.prototype.get.callCount, initialCallCount + 1);
+    }).end(done);
   });
 });
 
 describe('PUT /:file', function () {
   'use strict';
+  
+  before(function () {
+    sinon.stub(redis.RedisClient.prototype, 'set', function (key, data, callback) {
+      callback(null);
+    });
+  });
+
+
+  after(function () {
+    if (typeof redis.RedisClient.prototype.set.restore === 'function') {
+      redis.RedisClient.prototype.set.restore();
+    }
+  });
+  
   it('should return a 200 if a file is sent', function (done) {
-    supertest(app).put('/file').send(path.join(__dirname, '../../package.json')).expect(200).end(done);
+    var initialCallCount = redis.RedisClient.prototype.set.callCount;
+    supertest(app).put('/file').send(path.join(__dirname, '../../package.json')).expect(200).expect(function () {
+      assert.strictEqual(redis.RedisClient.prototype.set.callCount, initialCallCount + 1);
+    }).end(done);
   });
 
   it('should return a 400 if a file is not sent', function (done) {
-    supertest(app).put('/file').expect(400).end(done);
+    var initialCallCount = redis.RedisClient.prototype.set.callCount;
+    supertest(app).put('/file').expect(400).expect(function () {
+      assert.strictEqual(redis.RedisClient.prototype.set.callCount, initialCallCount);
+    }).end(done);
   });
 });
 
