86a93dfa914d0a1e0cfc2e7197b134778a51a872
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2015-12-15 10:42:00 +0100

    Add default route handling to avoid server crashes

--- app.js
+++ app.js
@@ -16,4 +16,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+app.use('*', function (req, res) {
+  'use strict';
+  res.status(404).send('No route defined for ' + req.path);
+});
+
 module.exports = app;
