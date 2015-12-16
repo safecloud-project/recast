7ca5a0efd564e9f2e15f796853be6bc234ce7e6a
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2015-12-16 11:46:55 +0100

    Modify simple_bench.lua to link with the proxy container

--- simple_bench.lua
+++ simple_bench.lua
@@ -5,14 +5,12 @@ function a_rand()
 end
 total_keys = tonumber(arg[1]) or 10
 size=arg[2] or "16"
-host = arg[3] or "192.168.99.104:3000"
+host = arg[3] or os.getenv("PROXY_PORT_3000_TCP_ADDR") .. ":" .. os.getenv("PROXY_PORT_3000_TCP_PORT")
 for i=1,total_keys do
 	--print("Putting data of size:",#v,misc.bitcalc(v).megabytes.." MB")
 	local next_key=a_rand()
-	--local p = "wget -q -O/dev/null  --method=PUT --body-data=\""..v.."...\" http://127.0.0.1:30001/"..next_key
 	local p = "wget -q -O/dev/null  --method=PUT --body-file=random"..size..".dat "..host.."/"..next_key
 	local p_short = "wget -q -O/dev/null  --method=PUT --body-file=random"..size..".dat "..host.."/"..next_key
 	print(i ,p_short)
 	os.execute(p)
 end
-
