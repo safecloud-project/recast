890ab58ba8ceb9ac28d70a3d389fb3812a5c29a9
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2015-12-17 14:14:33 +0100

    Naïve hammering bash client
    Changed the behaviour of the client so that it keeps trying to send
    files to the proxy until the proxy breaks.

--- client_bash/simple_bench.sh
+++ client_bash/simple_bench.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-#NEW_UUID=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 
 function random_key
 {
 	echo $(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 }
 
-#random_key=$(random_key)
-#echo $random_key
-
-TOTAL_KEYS=${1:-10} #1st param or default to 10
+UNUSED=${1:-10} #1st param left for argument order compatibility
 SIZE=${2:-16} #2nd param or default to 16
 HOST=${3:-"192.168.99.104:3000"} #os.getenv("PROXY_PORT_3000_TCP_ADDR") .. ":" .. os.getenv("PROXY_PORT_3000_TCP_PORT")
 
-echo "TOTAL_KEYS=${TOTAL_KEYS}"
-
-for (( i = 0; i < $TOTAL_KEYS; i++ )); do
+while [[ true ]]; do
 	wget -q -O/dev/null  --method=PUT --body-file=random${SIZE}.dat ${HOST}/$(random_key)
+	#rc=$?
+	#if [[ $rc -ne 0 ]]; then
+	#	echo "Exiting..."
+	#	exit 0
+	#fi
 done
-	
