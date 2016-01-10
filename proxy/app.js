8988fa70332f2d552ad920ce71b8a327183cc9b8
Dorian Burihabwa <dorian@burihabwa.com>
2016-01-10 15:19:42 +0100

    Use combined (apache style) logger for node.js proxy
    Use the 'combined' logger of morgan and store the records in a file at
    the root of the project called access.log.

--- proxy/app.js
+++ proxy/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var logger = require('morgan');
 
@@ -10,7 +11,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// logging
+var logFileOptions = {
+  'flags': 'a', //Work in append mode
+  'encoding': 'utf8',
+  'mode': 0666
+};
+var accessLogStream = fs.createWriteStream(path.join(__dirname, '/access.log'), logFileOptions);
+var logOptions = {
+  'stream': accessLogStream
+};
+app.use(logger('combined', logOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
