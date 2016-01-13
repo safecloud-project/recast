e08d7b31f0d5abff361f872769e4aaeeb9f606a6
Dorian Burihabwa <dorian@burihabwa.com>
2016-01-13 16:24:14 +0100

    Use Reed-Solomon(k=8, m=2) for encoding/decoding
    pycoder encoder/decoder now uses a Reed-Solomon(k=8, m=2) erasure coding to
    encode/decode the data it receives.

--- pycoder/coding_servicer.py
+++ pycoder/coding_servicer.py
@@ -1,12 +1,48 @@
+import logging
+
 from playcloud_pb2 import *
+from pyeclib.ec_iface import ECDriver
+
+
+class Eraser:
+    def __init__(self, k=8, m=2, ec_type="liberasurecode_rs_vand"):
+        self.k = k
+        self.m = m
+        self.ec_type = ec_type
+        self.driver = ECDriver(k=self.k, m=self.m, ec_type=self.ec_type)
+
+    def bytes_to_strips(self, data):
+        n = self.k + self.m
+        length = len(data) / n
+        strips = []
+        for i in range(n):
+            start = i * length
+            end = min(len(data), length + i * length)
+            strips.append(data[start:end])
+        return strips
+
+    def strips_to_bytes(self, strips):
+        return "".join(strips)
+
+    def encode(self, data):
+        strips = self.driver.encode(data)
+        return self.strips_to_bytes(strips)
+
+    def decode(self, data):
+        strips = self.bytes_to_strips(data)
+        return self.driver.decode(strips)
+
 
 class CodingService(BetaEncoderDecoderServicer):
+
     def Encode(self, request, context):
+        eraser = Eraser()
         reply = EncodeReply()
-        reply.enc_blocks = request.payload
+        reply.enc_blocks = eraser.encode(request.payload)
         return reply
 
     def Decode(self, request, context):
+        eraser = Eraser()
         reply = DecodeReply()
-        reply.dec_block = request.enc_blocks[0]
+        reply.dec_block = eraser.decode(request.enc_blocks[0])
         return reply
