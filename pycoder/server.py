bbe6a6c197c2acd129486074205120e0a2343495
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2016-01-14 19:22:16 +0100

    Print pycoder configuration on startup

--- pycoder/server.py
+++ pycoder/server.py
@@ -13,6 +13,10 @@ if __name__ == "__main__":
     PYCODER_LISTEN = PYCODER_LISTEN_ADDRESS + ":" + PYCODER_LISTEN_PORT
     SERVER = beta_create_EncoderDecoder_server(CodingService())
     SERVER.add_insecure_port(PYCODER_LISTEN)
+    print "starting server on"
+    print "k =", int(config.get("ec", "k"))
+    print "m =", int(config.get("ec", "m"))
+    print "type =", config.get("ec", "type")
     SERVER.start()
     try:
         while True:
