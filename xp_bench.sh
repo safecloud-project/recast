4b06941fe46283deed801637725a43c7d552b602
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2016-02-05 11:58:16 +0100

    Add client to deployment and benchmarking script
    Added a 4th machine to the deployment park and a benchmarking script to reload the
    nodes.

---
+++ xp_bench.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+source ./utils.sh
+
+function print_usage {
+  echo -e "Usage: ${0} <proxy-ip> <coder-ip> <redis-ip> <archive> <env-file> payload-size [payload-size [payload-size [...]]]"
+  echo -e ""
+  echo -e "Benchmark an encoder/decoder."
+  echo -e ""
+  echo -e "Arguments:"
+	echo -e "\tproxy-ip     IP address of the machine that will host the proxy instance"
+	echo -e "\tcoder-ip     IP address of the machine that will host the encoder/decoder instance"
+	echo -e "\tredis-ip     IP address of the machine that will host the redis instance"
+  echo -e "\tarchive      The archive containing the repo's code"
+	echo -e "\tenv-file     A file containing configuration values that the application should be using"
+  echo -e ""
+}
+
+if [[ "${#}" -lt 6 ]]; then
+  print_usage
+  exit 0
+fi
+
+readonly PROXY_HOST="${1}"
+shift
+readonly CODER_HOST="${1}"
+shift
+readonly REDIS_HOST="${1}"
+shift
+readonly ARCHIVE="${1}"
+shift
+readonly ENV_FILE="${1}"
+shift
+
+cd client_bash/
+docker build -t client .
+cd -
+mkdir -p xpdata/
+for BLOCK_SIZE in "$@"; do
+  setup_proxy "${PROXY_HOST}" "${ARCHIVE}" "${ENV_FILE}"
+  setup_coder "${CODER_HOST}" "${ARCHIVE}" "${ENV_FILE}"
+  setup_redis "${REDIS_HOST}" "${ARCHIVE}" "${ENV_FILE}"
+  source exports.source
+  docker run -it --rm -v "${PWD}/xpdata":/opt/xpdata client /bin/bash -c "cd /opt/xpdata && ls /home && /ab_playcloud.sh 1000 50 "${BLOCK_SIZE}" "${PROXY_PORT_3000_TCP_ADDR}:${PROXY_PORT_3000_TCP_PORT}""
+done
