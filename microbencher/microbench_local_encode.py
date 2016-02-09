9c2e9ee23e99f2b045279cd51a3b758448a73e7b
Dorian Burihabwa <dorian@burihabwa.com>
2016-02-09 10:08:32 +0100

    Cleanup pyeclib decode benchmarking script

--- microbencher/microbench_local_encode.py
+++ microbencher/microbench_local_encode.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+""" A script that encodes 1000 times a piece of data """
 
 import os
 import sys
@@ -7,6 +8,7 @@ import time
 from pyeclib.ec_iface import ECDriver
 
 def print_usage():
+    """Prints the usage message"""
     print "Usage:", sys.argv[0], " size"
     print ""
     print "Benchmark pyeclib erasure coding libraries"
@@ -14,28 +16,26 @@ def print_usage():
     print "Arguments:"
     print "\tsize       Size of the payload to encode in bytes"
 
-def gen_random_data(size):
-    return os.urandom(size)
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print_usage()
         sys.exit(0)
-    size = int(sys.argv[1])
-    data = gen_random_data(size)
-    requests = 1000
+    SIZE = int(sys.argv[1])
+    DATA = os.urandom(SIZE)
+    REQUESTS = 1000
 
     # Load driver
-    ec_k = int(os.environ.get("EC_K", 10))
-    ec_m = int(os.environ.get("EC_M", 4))
-    ec_type = os.environ.get("EC_TYPE", "liberasurecode_rs_vand")
-    driver = ECDriver(k=ec_k, m=ec_m, ec_type=ec_type)
+    EC_K = int(os.environ.get("EC_K", 10))
+    EC_M = int(os.environ.get("EC_M", 4))
+    EC_TYPE = os.environ.get("EC_TYPE", "liberasurecode_rs_vand")
+    DRIVER = ECDriver(k=EC_K, m=EC_M, EC_TYPE=EC_TYPE)
 
     # Start benchmark
-    print "About to encode ", requests, " payloads of size ", size, " bytes (", ec_type, ", k =",ec_k, ", m =", ec_m, ")"
-    for i in range(requests):
+    print "About to encode ", REQUESTS, " payloads of size ", SIZE, " bytes (", \
+    EC_TYPE, ", k =", EC_K, ", m =", EC_M, ")"
+    for i in range(REQUESTS):
         start = time.clock()
-        driver.encode(data)
+        DRIVER.encode(DATA)
         end = time.clock()
         elasped = (end - start) * 1000
         print elasped
