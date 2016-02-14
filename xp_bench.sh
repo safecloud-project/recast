07905a06f063dd9b919fce66a08c8cd3b9db9b8c
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2016-02-14 21:37:03 +0100

    Limit the number of concurrent requests performed by ab_playcloud
    Limit the number of concurrent requests of ab_playcloud, that was arbitrarily set to 50, to the number of cores * 2.

--- xp_bench.sh
+++ xp_bench.sh
@@ -32,6 +32,9 @@ shift
 readonly ENV_FILE="${1}"
 shift
 
+# Set the number of concurrent requests sent by ab_playcloud to # of cores * 2
+readonly CONCURRENT_REQUESTS="$(( $(grep -c ^processor /proc/cpuinfo) * 2 ))"
+
 cd client_bash/
 docker build -t client .
 cd -
@@ -44,6 +47,6 @@ for i in $(seq 10); do
     setup_redis "${REDIS_HOST}" "${ARCHIVE}" "${ENV_FILE}"
     source exports.source
     mkdir -p "${PWD}/xpdata/macrobench/${EC_TYPE}/${i}"
-    docker run -it --rm -v "${PWD}/xpdata":/opt/xpdata client /bin/bash -c "cd /opt/xpdata/macrobench/${EC_TYPE}/${i} && ls /home && /ab_playcloud.sh 1000 50 ${BLOCK_SIZE} ${PROXY_PORT_3000_TCP_ADDR}:${PROXY_PORT_3000_TCP_PORT} > stdout.txt"
+    docker run -it --rm -v "${PWD}/xpdata":/opt/xpdata client /bin/bash -c "cd /opt/xpdata/macrobench/${EC_TYPE}/${i} && ls /home && /ab_playcloud.sh 1000 ${CONCURRENT_REQUESTS} ${BLOCK_SIZE} ${PROXY_PORT_3000_TCP_ADDR}:${PROXY_PORT_3000_TCP_PORT} > stdout.txt"
   done
 done
