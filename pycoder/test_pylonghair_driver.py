01770da94332b3cbc8ba154d9faef97eac95b66b
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2016-02-23 02:58:49 +0100

    Use compatible block size to encode data with pylonghair

--- pycoder/test_pylonghair_driver.py
+++ pycoder/test_pylonghair_driver.py
@@ -1,9 +1,12 @@
 """
 Unit tests for pylonghair_driver
 """
+import os
+
 import pytest
 
 from pylonghair_driver import compute_block_size, split_and_number
+from pylonghair_driver import PylonghairDriver
 
 def test_remap_no_indices():
     data = [1, 2, 3, 4]
@@ -64,3 +67,33 @@ def test_compute_block_size_returns_8_if_block_size_is_lower_than_8():
 def test_compute_block_size_returns_a_multiple_of_8_if_initial_block_size_is_higher_than_8():
     block_size = compute_block_size(1024, 10)
     assert block_size == 104
+
+# Test PylonghairDriver encode
+DATA = os.urandom(1024)
+
+def test_encode():
+    k, m = 10, 4
+    expected_block_size = compute_block_size(len(DATA), k)
+    driver = PylonghairDriver(k=k, m=m)
+    strips = driver.encode(DATA)
+    assert isinstance(strips, list)
+    assert len(strips) == 14
+    # Test parity blocks have expected block size
+    expected_parity_strips = strips[len(strips) - m:]
+    assert m == len(expected_parity_strips)
+    for strip in expected_parity_strips:
+        assert expected_block_size == len(strip)
+
+def test_encode_less_than_k_data_blocks():
+    little_data = os.urandom(70)
+    k, m = 10, 4
+    expected_block_size = compute_block_size(len(little_data), k)
+    driver = PylonghairDriver(k=k, m=m)
+    strips = driver.encode(little_data)
+    assert isinstance(strips, list)
+    assert len(strips) == 13
+    # Test parity blocks have expected block size
+    expected_parity_strips = strips[len(strips) - m:]
+    assert m == len(expected_parity_strips)
+    for strip in expected_parity_strips:
+        assert expected_block_size == len(strip)
