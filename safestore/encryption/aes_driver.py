c0c29cb858e30e86ab626bea5f24b62614a8a679
Rogério Pontes <roger62@outlook.com>
2016-03-22 18:59:53 +0000

    AES Driver and Shamir Driver

---
+++ safestore/encryption/aes_driver.py
@@ -0,0 +1,35 @@
+
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+class AESDriver:
+
+    def __init__(self):
+        self.backend = default_backend()
+        """
+            AES 
+                128 bits - 16 bytes
+                192 bits - 24 bytes
+                256 bits - 32 bytes
+
+        """
+        self.key = os.urandom(32)
+        self.iv = os.urandom(16)
+        self.cipher = Cipher(algorithms.AES(
+            self.key), modes.CBC(self.iv), backend=default_backend())
+
+    def encode(self, data):
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(data)
+        padded_data += padder.finalize()
+        encryptor = self.cipher.encryptor()
+        secret = encryptor.update(padded_data) + encryptor.finalize()
+        return secret
+
+    def decode(self, data):
+        decryptor = self.cipher.decryptor()
+        dec = decryptor.update(data) + decryptor.finalize()
+        unpadder = padding.PKCS7(128).unpadder()
+        return unpadder.update(dec) + unpadder.finalize()
