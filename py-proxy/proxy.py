677b06106539779ac78a06357518115f6f04df08
Dorian Burihabwa <dorian@burihabwa.com>
2016-03-24 18:55:58 +0100

    Basic PUT route handler with bottle proxy implementation

--- py-proxy/proxy.py
+++ py-proxy/proxy.py
@@ -1,31 +1,13 @@
-# Simple Python-based PlayCloud proxy.py
-from wsgiref import simple_server
-import falcon
+from bottle import route, run, request
+import bottle
 
-class FileResource(object):
+bottle.BaseRequest.MEMFILE_MAX = 1024 * 1024 * 1024
 
-    @staticmethod
-    def on_get(req, resp):
-        resp.status = falcon.HTTP_200
-        resp.body = req.path
-        return resp
+@route("/<path>", method = "PUT")
+def read_data(path):
+    data = b""
+    if request.body.readable():
+        data = request.body.getvalue()
+    return str(len(data))
 
-    @staticmethod
-    def on_put(req, resp):
-        """Handles PUT requests"""
-        try:
-            data = req.stream.read()
-            resp.status = falcon.HTTP_200
-            resp.body = str(len(data)) + " bytes received"
-            return resp
-        except Exception as error:
-            resp.status = falcon.HTTP_500
-            resp.body = str(error)
-            return resp
-
-API = falcon.API(media_type="application/octet-stream")
-API.add_route("/", FileResource())
-
-if __name__ == "__main__":
-    HTTPD = simple_server.make_server("0.0.0.0", 8000, API)
-    HTTPD.serve_forever()
+run(host="0.0.0.0", port=8000)
