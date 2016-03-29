beb7952b866f7ca58b762c01aab38f23447258d7
Rogério Pontes <roger62@outlook.com>
2016-03-29 12:48:20 +0100

    Updated dockerfile and pycoder server.
    Added drive factory, still needs some work and tests.

--- pycoder/coding_servicer.py
+++ pycoder/coding_servicer.py
@@ -11,61 +11,124 @@ from playcloud_pb2 import DecodeReply
 from playcloud_pb2 import EncodeReply
 from playcloud_pb2 import Strip
 
+from safestore.encryption.xor_driver import XorDriver
+from safestore.encryption.hashed_xor_driver import HashedXorDriver, Hash, IntegrityException
+from safestore.encryption.signed_xor_driver import SignedXorDriver
+from safestore.encryption.signed_hashed_xor_driver import SignedHashedXorDriver
+from safestore.encryption.aes_driver import AESDriver
+from safestore.encryption.shamir_driver import ShamirDriver
+
 CONFIG = ConfigParser()
 CONFIG.read("pycoder.cfg")
 
-def convert_strips_to_bytes_list(playcloud_strips):
-    """Extract the data bytes from alist of playcloud strips"""
-    return [strip.data for strip in playcloud_strips]
+
+class DriverFactory():
+
+    def __init__(self):
+        self.driver = CONFIG.get("main", "grpc")
+        self.drivers = {'xor': self.xor,
+                        'hashed_xor_driver': self.hash_xor_driver,
+                        'signed_hashed_xor_driver': self.signed_hashed_xor_driver,
+                        'shamir': self.shamir,
+                        'signed_xor_driver': self.signed_xor_driver
+                        'erasure_driver': self.erasure_driver}
+
+    def xor(self):
+        return XorDriver(CONFIG.getint("xor", "n_blocks")
+
+    def hash_xor_driver(self):
+        nblocks=CONFIG.getint("hashed_xor_driver", "n_blocks")
+        hash=CONFIG.get("hashed_xor_driver", "hash")
+        return HashedXorDriver(nblocks, hash)
+
+    def signed_hashed_xor_driver(self):
+        nblocks=CONFIG.getint("signed_hashed_xor_driver", "n_blocks")
+        hash=CONFIG.get("signed_hashed_xor_driver", "hash")
+        return SignedHashedXorDriver(nblocks, hash)
+
+    def shamir(self):
+        nblocks=CONFIG.getint("shamir", "n_blocks")
+        threshold=CONFIG.getint("shamir", "threshold")
+        return Shamir(nblocks, hash)
+
+    def signed_xor_driver(self):
+        nblocks=CONFIG.getint("signed_xor_driver", "n_blocks")
+        return SignedXorDriver(nblocks)
+
+    def erasure_driver(self):
+        EC_K=int(os.environ.get("EC_K", CONFIG.get("ec", "k")))
+        EC_M=int(os.environ.get("EC_M", CONFIG.get("ec", "m")))
+        EC_TYPE=os.environ.get("EC_TYPE", CONFIG.get("ec", "type"))
+        return Eraser(EC_K, EC_M, EC_TYPE)
+
+    def get_driver(self):
+        return self.drivers[self.driver]
+
+
+
+
+
+
+def bytes_to_strips(k, m, payload):
+    """Transforms a byte string in a list of bytes string"""
+    disks=k + m
+    length=len(payload) / disks
+    strips=[]
+    for i in xrange(0, len(payload), length):
+        strips.append(payload[i: i + length])
+    return strips
+
+
+def strips_to_bytes(strips):
+    """Flatens a list of byte strings in single byte string"""
+    flattened_bytes=""
+    for strip in strips:
+        if isinstance(strip, bytearray):
+            flattened_bytes += str(strip)
+        else:
+            flattened_bytes += strip
+    return flattened_bytes
+
 
 class Eraser(object):
     """A wrapper for pyeclib erasure coding driver (ECDriver)"""
+
     def __init__(self, k=8, m=2, ec_type="liberasurecode_rs_vand"):
-        self.k = k
-        self.m = m
-        self.ec_type = ec_type
+        self.k=k
+        self.m=m
+        self.ec_type=ec_type
         if EC_TYPE == "pylonghair":
-            self.driver = PylonghairDriver(k=EC_K, m=EC_M, ec_type=EC_TYPE)
+            self.driver=PylonghairDriver(k=EC_K, m=EC_M, ec_type=EC_TYPE)
         elif EC_TYPE == "striping" or EC_TYPE == "bypass":
-            self.driver = ECStripingDriver(k=EC_K, m=0, hd=None)
+            self.driver=ECStripingDriver(k=EC_K, m=0, hd=None)
         else:
-            self.driver = ECDriver(k=EC_K, m=EC_M, ec_type=EC_TYPE)
+            self.driver=ECDriver(k=EC_K, m=EC_M, ec_type=EC_TYPE)
 
     def encode(self, data):
         """Encode a string of bytes in flattened string of byte strips"""
-        raw_strips = self.driver.encode(data)
-        strips = []
-        for raw_strip in raw_strips:
-            strip = Strip()
-            strip.data = raw_strip
-            strips.append(strip)
-        return strips
+        strips=self.driver.encode(data)
+        return strips_to_bytes(strips)
 
-    def decode(self, playcloud_strips):
-        """Decode byte strips in a string of bytes"""
-        strips = convert_strips_to_bytes_list(playcloud_strips)
+    def decode(self, data):
+        """Decode a flattened string of byte strips in a string of bytes"""
+        strips=bytes_to_strips(self.k, self.m, data)
         return self.driver.decode(strips)
 
-EC_K = int(os.environ.get("EC_K", CONFIG.get("ec", "k")))
-EC_M = int(os.environ.get("EC_M", CONFIG.get("ec", "m")))
-EC_TYPE = os.environ.get("EC_TYPE", CONFIG.get("ec", "type"))
-
-ERASER = Eraser(EC_K, EC_M, EC_TYPE)
 
 class CodingService(BetaEncoderDecoderServicer):
     """
     An Encoder/Decoder built on top of playcloud.proto that can be loaded by a
     GRPC server
     """
+
     def Encode(self, request, context):
         """Encode data sent in an EncodeRequest into a EncodeReply"""
-        reply = EncodeReply()
-        strips = ERASER.encode(request.payload)
-        reply.strips.extend(strips)
+        reply=EncodeReply()
+        reply.enc_blocks=ERASER.encode(request.payload)
         return reply
 
     def Decode(self, request, context):
         """Decode data sent in an DecodeRequest into a DecodeReply"""
-        reply = DecodeReply()
-        reply.dec_block = ERASER.decode(request.strips)
+        reply=DecodeReply()
+        reply.dec_block=ERASER.decode(request.enc_blocks)
         return reply
--- pycoder/server.py
+++ pycoder/server.py
@@ -1,4 +1,4 @@
-"""Main pycoder module that launches the GRPC server"""
+"""Main pycoder module that launches the GRPC server."""
 from ConfigParser import ConfigParser
 import os
 import time
@@ -9,16 +9,13 @@ from coding_servicer import CodingService
 if __name__ == "__main__":
     config = ConfigParser()
     config.read("pycoder.cfg")
-    PYCODER_LISTEN_ADDRESS = os.environ.get("GRPC_LISTEN_ADDRESS", config.get("grpc", "listen_address"))
-    PYCODER_LISTEN_PORT = os.environ.get("GRPC_LISTEN_PORT", config.get("grpc", "listen_port"))
+    PYCODER_LISTEN_ADDRESS = os.environ.get(
+        "GRPC_LISTEN_ADDRESS", config.get("main", "listen_address"))
+    PYCODER_LISTEN_PORT = os.environ.get(
+        "GRPC_LISTEN_PORT", config.get("main", "listen_port"))
     PYCODER_LISTEN = PYCODER_LISTEN_ADDRESS + ":" + PYCODER_LISTEN_PORT
     SERVER = beta_create_EncoderDecoder_server(CodingService())
     SERVER.add_insecure_port(PYCODER_LISTEN)
-    MESSAGE = "starting server on\n" + \
-    "k = " +    os.environ.get("EC_K", config.get("ec", "k")) + "\n" +\
-    "m = " +    os.environ.get("EC_M", config.get("ec", "m")) + "\n" +\
-    "type = " + os.environ.get("EC_TYPE", config.get("ec", "type")) + "\n"
-    print MESSAGE
     SERVER.start()
     try:
         while True:
