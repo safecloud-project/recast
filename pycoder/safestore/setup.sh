ef7427cb0a34fc954ec160aad30066e166f45bdb
Dorian Burihabwa <dorian@burihabwa.com>
2016-03-31 16:30:36 +0200

    Copy safestore source inside the py-proxy directory

--- pycoder/safestore/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pip install pycrypto
-sudo pip install dropbox
-sudo pip install --upgrade google-api-python-client
-sudo pip install python-onedrive
-sudo pip install watchdog
-sudo pip install futures
