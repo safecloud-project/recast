9376b3d6fca888c86bca2248d1a18e304796eb5e
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2016-04-01 19:10:33 +0200

    FIX: microbench scripts failing due to pycoder re-organization

--- pyeclib-decode-microbench.sh
+++ pyeclib-decode-microbench.sh
@@ -26,8 +26,7 @@ source "${ENV_FILE}"
 DATA_DIRECTORY="xpdata/pyeclib/$(basename "${EC_TYPE}")/"
 declare -a SIZES=("4" "16" "64")
 
-cp pycoder/pylonghair_driver.py microbencher/pylonghair_driver.py
-cp pycoder/custom_driver.py microbencher/custom_driver.py
+cp pycoder/custom_drivers.py microbencher/custom_drivers.py
 cp pycoder/__init__.py microbencher/__init__.py
 cd microbencher
 docker build -t pyeclib-microbencher -f pyeclib.Dockerfile .
@@ -36,9 +35,9 @@ mkdir -p "${DATA_DIRECTORY}"
 
 
 for size in "${SIZES[@]}"; do
-	ADJUSTED_SIZE="$((size * 1024 * 1024))"
+	PAYLOAD_SIZE_IN_MB="$((size * 1024 * 1024))"
 	for rep in $(seq "${REPETITIONS}"); do
 		docker run -it --rm -v "${PWD}/xpdata/pyeclib":/opt/xpdata/pyeclib pyeclib-microbencher \
-                bash -c "eval ${ENV_FILE_CONTENT} && /usr/local/src/app/microbench_local_decode.py ${ADJUSTED_SIZE} > /opt/$DATA_DIRECTORY/microbench-decode-${size}MB-${rep}.dat"
+                bash -c "eval ${ENV_FILE_CONTENT} && /usr/local/src/app/microbench_local_decode.py ${PAYLOAD_SIZE_IN_MB} > /opt/$DATA_DIRECTORY/microbench-decode-${size}MB-${rep}.dat"
 	done
 done
