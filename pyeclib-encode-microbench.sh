dd421754748d6f8b809a1616a272f68ad0771b9c
Rogério Pontes <roger62@outlook.com>
2016-04-02 13:36:05 +0100

    fixed folder name

--- pyeclib-encode-microbench.sh
+++ pyeclib-encode-microbench.sh
@@ -26,7 +26,9 @@ ENV_VARIABLES="$(cat ${ENV_FILE}  | sed -e s/export/-e/ | sed -e ':a;N;$!ba;s/\n
 REPETITIONS="${2}"
 REQUESTS=${3}
 source "${ENV_FILE}"
-DATA_DIRECTORY="results/microbench/encode/${DRIVER}"
+FOLDER=${ENV_FILE:4:-4}
+DATA_DIRECTORY="results/microbench/encode/${FOLDER}"
+
 echo $DATA_DIRECTORY
 
 declare -a SIZES=("4" "16" "64")
@@ -47,6 +49,8 @@ cd ../
 for size in "${PAYLOAD_SIZES[@]}"; do
 	PAYLOAD_SIZE_IN_MB="$((size * 1024 * 1024))"
 	for rep in $(seq "${REPETITIONS}"); do
+		#docker run -it --rm -v "${PWD}/${DATA_DIRECTORY}":/opt/$DATA_DIRECTORY pyeclib-microbencher \
+        #        bash -c "eval ${ENV_FILE_CONTENT} && /usr/local/src/app/microbench_local_encode.py ${ADJUSTED_SIZE} ${REQUESTS} > /opt/$DATA_DIRECTORY/microbench-encode-${size}MB-${rep}.dat"
 		docker run --interactive --tty --rm --volume "${PWD}/${DATA_DIRECTORY}":/opt/$DATA_DIRECTORY  ${ENV_VARIABLES} --entrypoint /usr/local/src/app/microbench_local_encode.py pycoder-microbench "${PAYLOAD_SIZE_IN_MB}" > $DATA_DIRECTORY/microbench-encode-${size}MB-${rep}.dat
 	done
 done
