b60d6a7fb9d6f39e66524669d24f1893cfef4ca5
Roger Pontes <rogerp062@outlook.com>
2016-04-15 16:13:09 +0100

    measure providers ping

--- py-proxy/misc.py
+++ py-proxy/misc.py
@@ -1,7 +1,11 @@
 from safestore.providers.one import ODrive
+from safestore.providers.gdrive import GDrive
+from safestore.providers.dbox import DBox
 
-
-drive = ODrive()
+drive = DBox()
+# onedrive 204.79.197.213
+# gdrive www.googleapis.com
+# dropbox content.dropboxapi.com
 
 drive.put("ola", "cenas.txt")
 print(drive.get("cenas.txt"))
