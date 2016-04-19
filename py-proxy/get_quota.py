10f0a9bb913dc6efe21e49bc8bf3fa72b92eed52
Dorian Burihabwa <dorian.burihabwa@unine.ch>
2016-04-19 20:58:56 +0200

    Add a script to get quota information from storage provider
    Added a script to get quota use information from a storage provider.
    
    For dropbox, google drive and onedrive, this script gives information
    about much space we have left before hitting th quota (in bytes).

---
+++ py-proxy/get_quota.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+"""
+Gets the amount of free space in at the providers in use by the py-proxy
+"""
+
+import json
+import os
+
+from safestore.providers.dispatcher import ProviderFactory
+
+
+CONFIGURATION_PATH = os.path.join(os.path.dirname(__file__), "./dispatcher.json")
+
+def get_quota_from_providers():
+    """
+    Clear the data in the storage providers listed in dispatcher.json
+    """
+    with open(CONFIGURATION_PATH, "r") as configuration_file:
+        configuration = json.load(configuration_file)
+    if not configuration.has_key('providers'):
+        raise Exception("the configuration should have a 'providers' key")
+    if not configuration.has_key('providers'):
+        raise Exception("the providers value should be an array")
+    provider_configurations = configuration.get('providers')
+    data = []
+    factory = ProviderFactory()
+    for provider_configuration in provider_configurations:
+        provider = factory.get_provider(provider_configuration)
+        provider_configuration["free_space"] = provider.quota()
+        data.append(provider_configuration)
+    return data
+
+if __name__ == "__main__":
+    print get_quota_from_providers()
