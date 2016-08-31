fe2cf4652b50db58c333305723729aa33a78e523
Dorian Burihabwa <dorian@burihabwa.com>
2016-08-31 17:41:22 +0200

    Display fresh stat data in fuse client by calling the proxy's API
    When the fuse client receives a request for metadata, it fetches fresh
    data from the proxy meta API (/<file>/__meta).

--- fuse/client.py
+++ fuse/client.py
@@ -1,8 +1,10 @@
+import copy
 import json
 import time
 
+import fuse
+import dateutil.parser
 import urllib3
-from fuse import FUSE, Operations
 
 class HTTPClient(object):
     """
@@ -21,6 +23,21 @@ class HTTPClient(object):
         response = self.http.request("GET", url)
         return response.data
 
+    def get_metadata(self, filename):
+        """
+        Returns a dictionary with the file's metadata
+        Args:
+            filename(string): path to the file
+        Returns:
+            dict: A dictionary with files metadata
+        """
+        if filename is None or len(filename) == 0:
+            raise ValueError("filename argument must have a value")
+        url = self.protocol + "://" + self.host + ":" + str(self.port) + filename + "/__meta"
+        response = self.http.request("GET", url)
+        metadata = json.loads(response.data)
+        return metadata
+
     def list(self):
         """
         List files stored in playcloud
@@ -37,7 +54,19 @@ ST_MODES = {
     "ROOT_DIRECTORY": 16893
 }
 
-class FuseClient(Operations):
+
+ROOT_STAT = {
+    "st_atime": time.time(),
+    "st_ctime": time.time(),
+    "st_mtime": time.time(),
+    "st_mode": ST_MODES["ROOT_DIRECTORY"],
+    "st_nlink": 1,
+    "st_size": 4096,
+    "st_uid": 1000,
+    "st_gid": 1000
+}
+
+class FuseClient(fuse.Operations):
 
     def __init__(self):
         self.client = HTTPClient(host="127.0.0.1", port=3000)
@@ -54,20 +83,31 @@ class FuseClient(Operations):
             yield ent
 
     def getattr(self, path, fh=None):
-        stat = {}
-        stat["st_atime"] = time.time()
-        stat["st_ctime"] = time.time()
-        stat["st_mtime"] = time.time()
-        stat["st_nlink"] = 1
-        stat["st_size"] = 4096
-        stat["st_uid"] = 1000
-        stat["st_gid"] = 1000
+        """
+        Returns metadata that can be used by unix's stat or ls.
+        The function returns empty dictionary in the case where the path is ./Trash or ./Trash-1000
+        Args:
+            path(string): Path to the file
+        Returns:
+            dict: A dictionary with values matching the file requested
+        """
+        # Ignore the trash directory that fuse looks up on mount
+        if path in ["/.Trash", "/.Trash-1000"]:
+            return {}
+        # If stat on root, then return the constant ROOT_STAT
         if path == "/":
-            stat["st_mode"] = ST_MODES["ROOT_DIRECTORY"]
-        else:
-            stat["st_mode"] = ST_MODES["OTHER_FILES"]
+            return ROOT_STAT
+        #Otherwise get fresh metadata from the system
+        metadata = self.client.get_metadata(path)
+        creation_date = int(dateutil.parser.parse(metadata["creation_date"]).strftime("%s"))
+        stat = copy.deepcopy(ROOT_STAT)
+        stat["st_atime"] = time.time()
+        stat["st_ctime"] = creation_date
+        stat["st_mtime"] = creation_date
+        stat["st_size"] = metadata["original_size"]
+        stat["st_mode"] = ST_MODES["OTHER_FILES"]
         return stat
 
 if __name__ == "__main__":
     CLIENT = FuseClient()
-    FUSE(CLIENT, "/tmp/fuse", nothreads=True, foreground=True)
+    fuse.FUSE(CLIENT, "/tmp/fuse", nothreads=True, foreground=True)
