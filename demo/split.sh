ae2ff130da800adb60c9764a51bda89c56d0b6b3
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2016-09-06 11:02:52 +0200

    demo files

--- demo/split.sh
+++ demo/split.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-declare -a PAYLOAD_SIZES=(1024 2048 131072 262144 4194304 8388608) #1K 2K 128K 256K 4MB 8MB
-INPUT="fuse.log"
+
+if [ -z "$1" ]
+  then
+    echo "No input file supplied"
+	exit 1; 
+fi
+
+declare -a PAYLOAD_SIZES=(1024 2048 131072 262144 524288 4194304 8388608) #1K 2K 128K 256K 4MB 8MB
+INPUT=$1
 for size in ${PAYLOAD_SIZES[@]}; do
-	#echo $size
 	grep "$size" $INPUT | nl > data/${size}_data.txt
 done
