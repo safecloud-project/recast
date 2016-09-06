8d9ed22439549c0159f10aa131af305310e54c22
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2016-09-06 16:11:18 +0200

    use vars to run the demo

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-input_file='fuse_used.log'
+demo='local_nodes'
+api='http'
+input_file="$demo/$api.log"
 requests=`wc -l $input_file | awk -F ' ' {'print $1'}`
 
 rm data/*
-gnuplot demo_local_fuse.gp
+gnuplot ${demo}_${api}.gp
 
-for (( i = 1; i < $requests; i++ )); do
+for (( i = 1; i <= $requests; i++ )); do
 	echo "Reading $i lines"
 	head -n $i $input_file > tmp_gnuplot_input
 	bash split.sh tmp_gnuplot_input
-	gnuplot demo_local_fuse.gp
+	gnuplot ${demo}_${api}.gp
 	sleep 0.25;
 done
