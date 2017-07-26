cf1b168c052e33ce62c428c5cda11a413512b2b3
Dorian Burihabwa <dorian@burihabwa.com>
2017-07-26 16:55:09 +0200

    Add script to dump playcloud dict to a pickle file

---
+++ attacks/dump_graph.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+"""
+A script that dumps the entanglement graph (in the form of a dict) from playcloud.
+"""
+import json
+import pickle
+import requests
+import os
+
+def fetch_entanglement_graph(host="127.0.0.1", port=3000, path="/dict"):
+    """
+    Returns:
+        dict: The entanglement graph
+    """
+    url = "http://{:s}:{:d}{:s}".format(host, port, path)
+    req = requests.get(url)
+    assert req.status_code == 200
+    graph = json.loads(req.text)
+    return graph
+
+
+def dump_entanglement_graph(graph):
+    """
+    Dumps the graph to a file
+    Args:
+        graph(dict): The entanglement graph
+    Returns:
+        str: The path to the dictionary
+    """
+    full_path = os.path.join("./dictionaries", "dict.pickle")
+    with open(full_path, "wb") as handle:
+         pickle.dump(graph, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return full_path
+     
+
+if __name__ == "__main__":
+    graph =  fetch_entanglement_graph()
+    print dump_entanglement_graph(graph)
