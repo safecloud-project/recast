c9883a638d9ad195fd6983106f5d5f422498354b
Dorian Burihabwa <dorian@burihabwa.com>
2017-08-24 15:58:06 +0200

    Fix: outdated tests

--- pyproxy/tests/test_dispatcher.py
+++ pyproxy/tests/test_dispatcher.py
@@ -1,9 +1,13 @@
-import math
+import copy
 import os
 
+import mock
 import pytest
 
-from pyproxy.safestore.providers.dispatcher import arrange_elements, Dispatcher, Metadata, ProviderFactory
+from pyproxy.files import Metadata
+from pyproxy.playcloud_pb2 import File
+from pyproxy.safestore.providers.dispatcher import arrange_elements, Dispatcher
+from pyproxy.safestore.providers.dispatcher import ProviderFactory
 from pyproxy.safestore.providers.redis_provider import RedisProvider
 
 DEFAULT_PATH = "hello"
@@ -18,80 +22,69 @@ VALID_DISPATCHER_CONF = {
     "entanglement": {
         "enabled": False
     },
-    "providers": [{"type": "redis", "host": "127.0.0.1", "port": 6379}]
+    "providers": {"redis": {"type": "redis", "host": "127.0.0.1", "port": 6379}}
 }
 
 def test_dispatcher_store_returns_a_Metadata_object():
-    dispatcher = Dispatcher(VALID_DISPATCHER_CONF)
-    meta = dispatcher.put(DEFAULT_PATH, BLOCKS)
-    assert type(meta) == Metadata
+    conf = copy.deepcopy(VALID_DISPATCHER_CONF)
+    del conf["providers"]["redis"]
+    dispatcher = Dispatcher(conf)
+    encoded_file = File()
+    encoded_file.original_size = 0
+    meta = dispatcher.put(DEFAULT_PATH, encoded_file)
+    assert isinstance(meta, Metadata)
     assert meta.path == DEFAULT_PATH
 
-
-def test_dispatcher_store_saves_Metadata_object():
-    dispatcher = Dispatcher(VALID_DISPATCHER_CONF)
-    meta = dispatcher.put(DEFAULT_PATH, BLOCKS)
-    assert DEFAULT_PATH in dispatcher.files
-    assert meta == dispatcher.files.get(DEFAULT_PATH)
 
 def test_ProdiverFactory_get_factory_raises_execption_if_no_type_in_configuration():
     configuration_without_type = {}
     factory = ProviderFactory()
     try:
         factory.get_provider(configuration_without_type)
-    except Exception as e:
-        assert(e.message == "configuration parameter must have a type key-value pair")
+    except Exception as error:
+        assert error.message == "configuration parameter must have a type key-value pair"
+
 
 def test_ProdiverFactory_get_factory_raises_execption_if_type_not_supported():
-    configuration_with_unsupported_type = { "type": "anUnsupportedType" }
+    configuration_with_unsupported_type = {"type": "anUnsupportedType"}
     factory = ProviderFactory()
     try:
         factory.get_provider(configuration_with_unsupported_type)
-    except Exception as e:
-        assert(e.message == "configuration type is not supported by the factory")
+    except Exception as error:
+        assert error.message == "configuration type is not supported by the factory"
+
 
 def test_ProdiverFactory_get_factory_returns_a_provider_if_supported():
-    VALID_DISPATCHER_CONF = {"type": "redis", "host": "127.0.0.1", "port": 6379}
+    valid_provider_conf = {"type": "redis", "host": "127.0.0.1", "port": 6379}
     factory = ProviderFactory()
-    provider = factory.get_provider(VALID_DISPATCHER_CONF)
+    provider = factory.get_provider(valid_provider_conf)
     assert isinstance(provider, RedisProvider)
 
+
 def test_arrange_elements_raises():
-    with pytest.raises(ValueError) as error:
+    with pytest.raises(ValueError):
         arrange_elements(2, -1)
 
-    with pytest.raises(ValueError) as error:
+    with pytest.raises(ValueError):
         arrange_elements(-1, 2)
 
-    with pytest.raises(ValueError) as error:
+    with pytest.raises(ValueError):
         arrange_elements(-1, -1)
 
+
 def test_Dispatcher_list_files():
-    DISPATCHER_CONF = {"providers": []}
-    dispatcher = Dispatcher(DISPATCHER_CONF)
-    result = dispatcher.list()
+    conf = copy.deepcopy(VALID_DISPATCHER_CONF)
+    del conf["providers"]["redis"]
+    dispatcher = Dispatcher(conf)
+    with mock.patch("pyproxy.safestore.providers.dispatcher.Files.values", return_value=[]):
+        result = dispatcher.list()
     assert isinstance(result, list)
     assert len(result) == 0
-    PATH = "key"
-    DATA = "DATA"
-    dispatcher.put(PATH, DATA)
-    result = dispatcher.list()
+    path = "key"
+    metadata = Metadata(path)
+    with mock.patch("pyproxy.safestore.providers.dispatcher.Files.values", return_value=[metadata]):
+        result = dispatcher.list()
     assert isinstance(result, list)
     assert len(result) == 1
     assert isinstance(result[0], Metadata)
-    assert result[0].path == PATH
-
-
-def test_Dispatcher_sets_original_size_when_passed():
-    DISPATCHER_CONF = {"providers": []}
-    dispatcher = Dispatcher(DISPATCHER_CONF)
-    PATH = "key"
-    DATA = "DATA"
-    dispatcher.put(PATH, DATA)
-    meta = dispatcher.files[PATH]
-    assert meta.original_size == 0
-    CUSTOM_PATH = "key_with_custom_original_size"
-    CUSTOM_ORIGINAL_SIZE = 16 * int(math.pow(2, 20))
-    dispatcher.put(CUSTOM_PATH, DATA, original_size = CUSTOM_ORIGINAL_SIZE)
-    custom_meta = dispatcher.files[CUSTOM_PATH]
-    assert custom_meta.original_size == CUSTOM_ORIGINAL_SIZE
+    assert result[0].path == path
