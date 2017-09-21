d4a1d5f6719cd30e735c4d6acf92a083204ab9d9
Dorian Burihabwa <dorian@burihabwa.com>
2017-09-21 23:39:44 +0200

    Remove old pycoder files

--- pycoder/drivers/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DRIVERS_MODULE_DIRECTORY = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
-DRIVER_FILES = [os.path.join(DRIVERS_MODULE_DIRECTORY, f) for f in os.listdir(DRIVERS_MODULE_DIRECTORY)]
-MODULES = [f for f in DRIVER_FILES if os.path.isfile(f) and f.endswith("_driver.py") and not "test" in f and not f.endswith("__init__.py")]
-TRIMMED_MODULES = [f.replace(".py", "").replace("/", ".") for f in MODULES]
-__all__ = map(__import__, TRIMMED_MODULES)
