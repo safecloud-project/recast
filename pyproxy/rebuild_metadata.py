84c4a79e34ab21ab1f53687f09720a8debf19714
Dorian Burihabwa <dorian@burihabwa.com>
2017-11-02 10:03:18 +0100

    Split metadata reconstruction function in more granular functions

--- pyproxy/rebuild_metadata.py
+++ pyproxy/rebuild_metadata.py
@@ -10,11 +10,51 @@ import os
 import pyproxy.safestore.providers.dispatcher as d
 import pyproxy.metadata as metadata
 
-
 DEFAULT_DISPATCHER_CONFIGURATION = os.path.join(os.path.dirname(__file__), "dispatcher.json")
+
+
+def rebuild_node(provider, provider_name, documents=None):
+    """
+    Rebuilds metadata by reading data from a given storage node.
+    Args:
+        provider(Provider): Storage provider
+        provider_name(str): Name of the provider
+        documents(str, metadata.MetaDocument): The current list of MetaDocuments
+    Returns:
+        dict(str, metadata.MetaDocument): The list of MetaDocuments built from the database
+    """
+    if not provider:
+        raise ValueError("provider argument must be a valid Provider with a list and a get method")
+    if not provider_name or not isinstance(provider_name, (str, unicode)):
+        raise ValueError("provider_name argument must be a non empty string")
+    documents = documents or {}
+    for block_id in provider.list():
+        document_name = d.extract_path_from_key(block_id)
+        document = documents.get(document_name, None)
+        if not document:
+            document = metadata.MetaDocument(document_name)
+        metablock = None
+        for a_metablock in document.blocks:
+            if a_metablock.key == block_id:
+                metablock = a_metablock
+                break
+        if not metablock:
+            data = provider.get(block_id)
+            checksum = hashlib.sha256(data).digest()
+            if not document.entangling_blocks:
+                document.entangling_blocks = metadata.extract_entanglement_data(data)
+            if not document.original_size:
+                document.original_size = metadata.extract_document_size(data)
+            metablock = metadata.MetaBlock(block_id, checksum=checksum, size=len(data))
+            document.blocks.append(metablock)
+        metablock.providers = list(set(metablock.providers + [provider_name]))
+        documents[document_name] = document
+    return documents
+
 
 def rebuild(configuration_path):
     """
+    Rebuilds metadata by data from the storage nodes
     Args:
         configuration_path(str): Path to the dispatcher configuration file
     Returns:
@@ -29,28 +69,9 @@ def rebuild(configuration_path):
     documents = {}
     for provider_name in dispatcher.providers:
         provider = dispatcher.providers[provider_name]
-        for block_id in provider.list():
-            document_name = d.extract_path_from_key(block_id)
-            document = documents.get(document_name, None)
-            if not document:
-                document = metadata.MetaDocument(document_name)
-            metablock = None
-            for a_metablock in document.blocks:
-                if a_metablock.key == block_id:
-                    metablock = a_metablock
-                    break
-            if not metablock:
-                data = provider.get(block_id)
-                checksum = hashlib.sha256(data).digest()
-                if not document.entangling_blocks:
-                    document.entangling_blocks = metadata.extract_entanglement_data(data)
-                if not document.original_size:
-                    document.original_size = metadata.extract_document_size(data)
-                metablock = metadata.MetaBlock(block_id, checksum=checksum, size=len(data))
-                document.blocks.append(metablock)
-            metablock.providers = list(set(metablock.providers + [provider_name]))
-            documents[document_name] = document
+        documents = rebuild_node(provider, provider_name, documents=documents)
     return documents
+
 
 def write_metadata(host, port, documents):
     """
@@ -104,11 +125,13 @@ def write_metadata(host, port, documents):
             for inserted in last_inserts:
                 documents_group.remove(inserted)
 
+
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(__file__, description="A script to rebuild the metadata in a different server")
     PARSER.add_argument("--host", type=str, required=True, help="Host of the new metadata server")
     PARSER.add_argument("--port", type=int, required=True, help="Port of the new metadata server")
-    PARSER.add_argument("--conf", type=str, help="Path to the dispatcher configuration", default=DEFAULT_DISPATCHER_CONFIGURATION)
+    PARSER.add_argument("--conf", type=str, help="Path to the dispatcher configuration",
+                        default=DEFAULT_DISPATCHER_CONFIGURATION)
 
     ARGS = PARSER.parse_args()
 
