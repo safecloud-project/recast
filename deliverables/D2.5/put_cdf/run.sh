f7a68918cd285878afe95c2e58f3e307ae6f1b03
Dorian Burihabwa <dorian@burihabwa.com>
2017-11-19 13:35:32 +0100

    FIX: redis deployment scripts

--- deliverables/D2.5/put_cdf/run.sh
+++ deliverables/D2.5/put_cdf/run.sh
@@ -15,7 +15,7 @@ main() {
 		for run in $(seq 1 "${RUNS}"); do
 			local result_directory="${experiment_directory}/${run}"
 			mkdir -p "${result_directory}"
-			ssh -o "StrictHostKeyChecking no" "${MAIN_HOST}" "cd playcloud && docker-compose stop && docker-compose rm -fv && sudo find volumes/ -type f -delete && docker-compose build && ./deploy_redis.sh && docker-compose up -d && sleep 12"
+			ssh -o "StrictHostKeyChecking no" "${MAIN_HOST}" "cd playcloud && docker-compose stop && docker-compose rm -fv && sudo find volumes/ -type f -delete && docker-compose build && ./deploy_redis.sh ./ips.txt && docker-compose up -d && sleep 12"
 			
 			../../../clients/ab_playcloud.sh 500 4 1 172.16.0.138:3000 | tee out.txt
 			mv gnuplot-* completion-* out.txt "${result_directory}"
--- deliverables/D2.5/put_cdf/step_no_replication/run.sh
+++ deliverables/D2.5/put_cdf/step_no_replication/run.sh
@@ -6,7 +6,7 @@ for i in $(seq 3 5); do
 	scp dispatcher.json 172.16.0.138:playcloud/pyproxy/
 	cd "${i}"
 	echo "${PWD}"
-	ssh -o "StrictHostKeyChecking no" 172.16.0.138 "cd playcloud && docker-compose stop && docker-compose rm -fv && docker-compose build && ./deploy_redis.sh && docker-compose up -d && sleep 12"
+	ssh -o "StrictHostKeyChecking no" 172.16.0.138 "cd playcloud && docker-compose stop && docker-compose rm -fv && docker-compose build && ./deploy_redis.sh ./ips.txt && docker-compose up -d && sleep 12"
 	../../../../../clients/ab_playcloud.sh 1000 4 1 172.16.0.138:3000 > out.txt
 	cd "${OLDPWD}"
 done
