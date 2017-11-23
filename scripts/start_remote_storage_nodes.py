06bc40270e5ca629e3e05aabca96876c39d9df40
Dorian Burihabwa <dorian@burihabwa.com>
2017-11-23 17:17:32 +0100

    Parallelize start and stop commands of remote storage nodes
    In order to speed up re-deployment processes

--- scripts/start_remote_storage_nodes.py
+++ scripts/start_remote_storage_nodes.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import re
 import subprocess
+import threading
 
 import configure
 
@@ -18,66 +19,97 @@ PATH_TO_DISPATCHER_CONF = os.path.join(os.path.dirname(__file__),
                                        "pyproxy/dispatcher.json")
 MINIO_ENDPOINT_PATTERN = re.compile(r"^http://(.+):(\d+)/?$")
 
-def start_minio(provider, provider_conf, ssh_port=22):
+
+START_MINIO_TEMPLATE = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
+                        "docker ps -aq | xargs --no-run-if-empty docker rm -fv && " +
+                        "docker pull minio/minio && " +
+                        "docker run --detach=true --name {:s} " +
+                        "--publish={:d}:{:d} " +
+                        "-e 'MINIO_ACCESS_KEY={:s}' -e 'MINIO_SECRET_KEY={:s}' " +
+                        "--volume ~/playcloud/volumes/{:s}:/data " +
+                        "minio/minio server --address '0.0.0.0:9000' /data")
+START_REDIS_TEMPLATE = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
+                        "docker ps -aq | xargs --no-run-if-empty docker rm -fv && " +
+                        "docker pull redis:3.2.8 && " +
+                        "docker run --detach=true --name {:s} " +
+                        "--publish={:d}:{:d} " +
+                        "--volume ~/playcloud/volumes/{:s}:/data " +
+                        "redis")
+
+class MinioStarter(threading.Thread):
     """
     Remotely starts a minio node through SSH
-    Args:
-        provider(str): Name of the storage node
-        provider_conf(dict): Configuration of the provider
-        ssh_port(int): Port number used by the SSH server on host
-    Returns:
-        int: Return code of the SSH command
     """
-    endpoint_url = provider_conf["endpoint_url"]
-    groups = MINIO_ENDPOINT_PATTERN.match(endpoint_url).groups()
-    minio_host = groups[0]
-    minio_port = int(groups[1])
-    minio_aws_access_key_id = provider_conf["aws_access_key_id"]
-    minio_aws_secret_access_key = provider_conf["aws_secret_access_key"]
-    command = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
-               "docker ps -aq | xargs --no-run-if-empty docker rm -fv && " +
-               "docker pull minio/minio:latest && " +
-               "docker run --detach=true --name {:s} " +
-               "--publish={:d}:{:d} " +
-               "-e 'MINIO_ACCESS_KEY={:s}' -e 'MINIO_SECRET_KEY={:s}' " +
-               "--volume ~/playcloud/volumes/{:s}:/data " +
-               "minio/minio server --address '0.0.0.0:9000' /data").format(ssh_port,
-                                                  minio_host,
-                                                  provider,
-                                                  minio_port,
-                                                  minio_port,
-                                                  minio_aws_access_key_id,
-                                                  minio_aws_secret_access_key,
-                                                  provider)
-    ret = subprocess.call(command.split())
-    return ret
+    def __init__(self, provider, provider_conf, ssh_port=22):
+        """
+        Args:
+            provider(str): Name of the storage node
+            provider_conf(dict): Configuration of the provider
+            ssh_port(int): Port number used by the SSH server on host
+        """
+        super(MinioStarter, self).__init__()
+        endpoint_url = provider_conf["endpoint_url"]
+        groups = MINIO_ENDPOINT_PATTERN.match(endpoint_url).groups()
+        minio_host = groups[0]
+        minio_port = int(groups[1])
+        minio_aws_access_key_id = provider_conf["aws_access_key_id"]
+        minio_aws_secret_access_key = provider_conf["aws_secret_access_key"]
+        self.host = minio_host
+        self.command = START_MINIO_TEMPLATE.format(ssh_port,
+                                                   minio_host,
+                                                   provider,
+                                                   minio_port,
+                                                   minio_port,
+                                                   minio_aws_access_key_id,
+                                                   minio_aws_secret_access_key,
+                                                   provider)
 
-def start_redis(provider, provider_conf, ssh_port=22):
+    def run(self):
+        """
+        Remotely starts a minio node through SSH
+        Returns:
+            int: Return code of the SSH command
+        """
+        ret = subprocess.call(self.command.split())
+        if ret == 0:
+            print "Successfully started node {:s}".format(self.host)
+        else:
+            print "Startup of node {:s} ended with {:d} return code".format(self.host, ret)
+        return ret
+
+class RedisStarter(threading.Thread):
     """
     Remotely starts a redis node through SSH
-    Args:
-        provider(str): Name of the storage node
-        provider_conf(dict): Configuration of the provider
-        ssh_port(int): Port number used by the SSH server on host
-    Returns:
-        int: Return code of the SSH command
     """
-    redis_host = provider_conf["host"]
-    redis_port = int(provider_conf["port"])
-    command = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
-               "docker ps -aq | xargs --no-run-if-empty docker rm -fv && " +
-               "docker pull minio/minio:latest && " +
-               "docker run --detach=true --name {:s} " +
-               "--publish={:d}:{:d} " +
-               "--volume ~/playcloud/volumes/{:s}:/data " +
-               "minio/minio server /data").format(ssh_port,
-                                                  redis_host,
-                                                  provider,
-                                                  redis_port,
-                                                  redis_port,
-                                                  provider)
-    ret = subprocess.call(command.split())
-    return ret
+    def __init__(self, provider, provider_conf, ssh_port=22):
+        """
+        Args:
+            provider(str): Name of the storage node
+            provider_conf(dict): Configuration of the provider
+            ssh_port(int): Port number used by the SSH server on host
+        """
+        super(RedisStarter, self).__init__()
+        redis_host = provider_conf["host"]
+        redis_port = int(provider_conf["port"])
+        self.host = redis_host
+        self.command = START_REDIS_TEMPLATE.format(ssh_port,
+                                                   redis_host,
+                                                   provider,
+                                                   redis_port,
+                                                   redis_port,
+                                                   provider)
+    def run(self):
+        """
+        Remotely starts a redis node through SSH
+        Returns:
+            int: Return code of the SSH command
+        """
+        ret = subprocess.call(self.command.split())
+        if ret == 0:
+            print "Successfully started node {:s}".format(self.host)
+        else:
+            print "Startup of node {:s} ended with {:d} return code".format(self.host, ret)
+        return ret
 
 def start(configuration):
     """
@@ -86,14 +118,19 @@ def start(configuration):
         configuration(dict): Dispatcher configuration
     """
     providers = configuration["providers"]
+    starters = []
     for provider in providers:
         provider_type = providers[provider]["type"]
         if provider_type == "s3":
-            start_minio(provider, providers[provider])
+            starters.append(MinioStarter(provider, providers[provider]))
         elif provider_type == "redis":
-            start_redis(provider, providers[provider])
+            starters.append(MinioStarter(provider, providers[provider]))
         else:
             raise RuntimeError("Unsupported type of provider")
+    for starter in starters:
+        starter.start()
+    for starter in starters:
+        starter.join()
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(__file__, description=__SCRIPT_DESCRIPTION)
--- scripts/stop_remote_storage_nodes.py
+++ scripts/stop_remote_storage_nodes.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import re
 import subprocess
+import threading
 
 import configure
 
@@ -18,40 +19,68 @@ PATH_TO_DISPATCHER_CONF = os.path.join(os.path.dirname(__file__),
                                        "pyproxy/dispatcher.json")
 MINIO_ENDPOINT_PATTERN = re.compile(r"^http://(.+):(\d+)/?$")
 
-def stop_minio(provider_conf, ssh_port=22):
+
+STOP_CONTAINER_TEMPLATE = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
+                           "docker ps -aq | xargs --no-run-if-empty docker rm -fv")
+
+class MinioStopper(threading.Thread):
     """
     Remotely stops a minio node through SSH
-    Args:
-        provider_conf(dict): Configuration of the provider
-        ssh_port(int): Port number used by the SSH server on host
-    Returns:
-        int: Return code of the SSH command
     """
-    endpoint_url = provider_conf["endpoint_url"]
-    groups = MINIO_ENDPOINT_PATTERN.match(endpoint_url).groups()
-    minio_host = groups[0]
-    command = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
-               "docker ps -aq | xargs --no-run-if-empty docker rm -fv").format(ssh_port,
-                                                                               minio_host)
-    ret = subprocess.call(command.split())
-    return ret
+    def __init__(self, provider_conf, ssh_port=22):
+        """
+        Args:
+            provider_conf(dict): Configuration of the provider
+            ssh_port(int): Port number used by the SSH server on host
+        """
+        super(MinioStopper, self,).__init__()
+        endpoint_url = provider_conf["endpoint_url"]
+        groups = MINIO_ENDPOINT_PATTERN.match(endpoint_url).groups()
+        minio_host = groups[0]
+        self.host = minio_host
+        self.command = STOP_CONTAINER_TEMPLATE.format(ssh_port, minio_host)
 
-def stop_redis(provider_conf, ssh_port=22):
+    def run(self):
+        """
+        Remotely stops a minio node through SSH
+        Returns:
+            int: Return code of the SSH command
+        """
+        ret = subprocess.call(self.command.split())
+        if ret == 0:
+            print "Successfully stopped node {:s}".format(self.host)
+        else:
+            print "Shutdown of node {:s} ended with {:d} return code".format(self.host, ret)
+        return ret
+
+class RedisStopper(threading.Thread):
     """
     Remotely stops a redis node through SSH
-    Args:
-        provider(str): Name of the storage node
-        provider_conf(dict): Configuration of the provider
-        ssh_port(int): Port number used by the SSH server on host
-    Returns:
-        int: Return code of the SSH command
     """
-    redis_host = provider_conf["host"]
-    command = ("ssh -o StrictHostKeyChecking=no -p {:d} {:s} " +
-               "docker ps -aq | xargs --no-run-if-empty docker rm -fv").format(ssh_port,
-                                                                               redis_host)
-    ret = subprocess.call(command.split())
-    return ret
+    def __init__(self, provider_conf, ssh_port=22):
+        """
+        Args:
+            provider_conf(dict): Configuration of the provider
+            ssh_port(int): Port number used by the SSH server on host
+        """
+        super(RedisStopper, self,).__init__()
+        redis_host = provider_conf["host"]
+        self.host = redis_host
+        self.command = STOP_CONTAINER_TEMPLATE.format(ssh_port, redis_host)
+
+    def run(self):
+        """
+        Remotely stops a redis node through SSH
+        Returns:
+            int: Return code of the SSH command
+        """
+        ret = subprocess.call(self.command.split())
+        if ret == 0:
+            print "Successfully stopped node {:s}".format(self.host)
+        else:
+            print "Shutdown of node {:s} ended with {:d} return code".format(self.host, ret)
+        return ret
+
 
 def start(configuration):
     """
@@ -60,14 +89,19 @@ def start(configuration):
         configuration(dict): Dispatcher configuration
     """
     providers = configuration["providers"]
+    stoppers = []
     for provider in providers:
         provider_type = providers[provider]["type"]
         if provider_type == "s3":
-            stop_minio(providers[provider])
+            stoppers.append(MinioStopper(providers[provider]))
         elif provider_type == "redis":
-            stop_redis(providers[provider])
+            stoppers.append(RedisStopper(providers[provider]))
         else:
             raise RuntimeError("Unsupported type of provider")
+    for stopper in stoppers:
+        stopper.start()
+    for stopper in stoppers:
+        stopper.join()
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(__file__, description=__SCRIPT_DESCRIPTION)
