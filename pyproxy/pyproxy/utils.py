1c82423e41ae07d9750a5ac2565e2ddba0f3bd61
Dorian Burihabwa <dorian@burihabwa.com>
2018-01-31 16:45:27 +0100

    Add util functions to get CPU count information

--- pyproxy/pyproxy/utils.py
+++ pyproxy/pyproxy/utils.py
@@ -2,9 +2,36 @@
 Utility methods
 """
 import logging
+import multiprocessing
+import subprocess
 import time
 
 import kazoo.client
+
+def get_threads_per_core():
+    """
+    Gets the number of threads per core from `lscpu` output.
+    Returns:
+        int: number of threads per core from `lscpu` output
+    Raises:
+        EnvironmentError: If the information cannot be found in the lscpu output
+    """
+    process = subprocess.Popen("lscpu", stdout=subprocess.PIPE)
+    out, _ = process.communicate()
+    for line in out.split("\n"):
+        if line.startswith("Thread(s) per core:"):
+            return int(line.split(":")[1])
+    raise EnvironmentError("Could not read number of threads per core from lscpu output")
+
+def get_max_threads():
+    """
+    Gets the number of threads that can run in parallel on the machine.
+    Returns:
+        int: number of threads that can run in parallel on the machine
+    Raises:
+        EnvironmentError: If the number of threads per core cannot be read
+    """
+    return multiprocessing.cpu_count() * get_threads_per_core()
 
 def init_zookeeper_client(host="zoo1", port=2181, max_retries=5):
     """
