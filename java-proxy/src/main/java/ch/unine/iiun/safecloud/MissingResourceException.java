61dd22f1641075ba6e464250607d828c28fc8d95
Dorian Burihabwa <dorian@burihabwa.com>
2018-03-06 15:35:48 +0100

    Remove old data and scripts

--- java-proxy/src/main/java/ch/unine/iiun/safecloud/MissingResourceException.java
+++
@@ -1,7 +0,0 @@
-package ch.unine.iiun.safecloud;
-
-public class MissingResourceException extends Exception {
-    public MissingResourceException(String message) {
-        super(message);
-    }
-}
